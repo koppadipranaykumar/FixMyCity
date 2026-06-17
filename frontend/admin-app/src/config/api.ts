b1e2012557a36dae35e3d8846191d5da081ebfb9
Pranay Kumar <koppadipranaykumar@gmail.com>
2026-06-17 16:40:15 +0530

    update the api.ts

--- frontend/admin-app/src/config/api.ts
+++ frontend/admin-app/src/config/api.ts
@@ -1,4 +1,4 @@
-
-const API_BASE_URL ="https://c0c679cd-f0e6-47aa-ba87-f6e48d432c3d-00-3rikrql6o8j8s.sisko.replit.dev";
+const API_BASE_URL =
+  "https://c0c679cd-f0e6-47aa-ba87-f6e48d432c3d-00-3rikrql6o8j8s.sisko.replit.dev";
 
 export default API_BASE_URL;
