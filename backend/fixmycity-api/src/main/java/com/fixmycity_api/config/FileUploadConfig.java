a230fab371594e077087339b58421a197ecbddc9
Pranay Kumar <koppadipranaykumar@gmail.com>
2026-06-18 15:32:17 +0530

    update the file upload code

--- backend/fixmycity-api/src/main/java/com/fixmycity_api/config/FileUploadConfig.java
+++ backend/fixmycity-api/src/main/java/com/fixmycity_api/config/FileUploadConfig.java
@@ -3,15 +3,19 @@ package com.fixmycity_api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class FileUploadConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(
-            ResourceHandlerRegistry registry) {
-
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Step up two directories out of backend/fixmycity-api to reach the main workspace root folder
+        Path rootPath = Paths.get("../../uploads").toAbsolutePath().normalize();
+        String uploadPath = rootPath.toUri().toString();
+        
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations(uploadPath);
     }
 }
